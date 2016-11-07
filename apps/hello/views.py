d0dbdf3bd623b226eff15c33af46c35d40dc582e
Alex Strutynskyy <kominterna62@gmail.com>
2016-11-07 10:58:15 +0200

    ticket:1 hardcoded home view

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    bio = {
+        'first_name': 'Oleksiy',
+        'last_name': 'Strutynskyy',
+        'birthday': 'May 7, 1979',
+        'photo': '',
+        'email': 'k6_alexstr@ukr.net',
+        'jabber': 'alexleon@42cc.co',
+        'skype': 'oleksiy.strutynskyy',
+        'contacts': 'facebook.com\n\
+                     linkedin.com\n\
+                     gmail.com',
+        'bio': 'I want to be Django Developer.\n\
+                Django junior is my first aim.'
+    }
+
+    return render(request, 'home.html', {'bio': bio})
