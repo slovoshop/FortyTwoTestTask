8cb1ffd331a78eec43e9ac526e69bec45f153173
Alex Strutynskyy <kominterna62@gmail.com>
2016-11-07 14:00:35 +0200

    ticket:3 make requests mocking generator

--- assets/js/requests.js
+++ assets/js/requests.js
@@ -1,8 +1,19 @@
 
 var $initTitle = $('title').text();
+var checkReqTmr; // timer for checking request's logs
+var unread = 0;
 
 
 window.onfocus = function() {
+  clearTimeout(checkReqTmr);
   $('title').text($initTitle);
+  unread = 0;
 };
+
+window.onblur = function() {
+	checkReqTmr = setInterval(function() {
+      unread++;
+      $(document).attr("title", "(" + unread + ") unread");
+    }, 1500);
+}
 
