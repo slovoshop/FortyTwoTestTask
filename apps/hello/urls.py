4c13e9680f72adf46e291c483d293ca00becb4ad
Alex Strutynskyy <kominterna62@gmail.com>
2016-11-07 13:30:06 +0200

    ticket:3 static request.html

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'apps.hello.views',
     url(r'^$', 'home', name='home'),
+    url(r'^request/$', 'hard_coded_requests', name='request'),
 )
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -6,3 +6,7 @@ def home(request):
     bio = AboutMe.objects.first()
 
     return render(request, 'home.html', {'bio': bio})
+
+
+def hard_coded_requests(request):
+    return render(request, 'request.html', {'num': range(10)})
