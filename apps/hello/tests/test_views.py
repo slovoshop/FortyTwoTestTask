5f5b84063e944299347a9263f17eff09d7bc17ee
Alex Strutynskyy <kominterna62@gmail.com>
2016-11-07 12:49:22 +0200

    ticket:1 refactor home view to work with AboutMe model

--- apps/hello/tests/test_views.py
+++ apps/hello/tests/test_views.py
@@ -1,7 +1,17 @@
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
+from apps.hello.models import AboutMe
 
-# Create your tests here.
+NORMAL = {
+    'first_name': 'Alex',
+    'last_name': 'Strong',
+    'birthday': '1979-09-09',
+    'email': 'k6_alexstr@ukr.net',
+    'jabber': 'jabber',
+    'skype': 'skype',
+    'bio': 'bio',
+    'contacts': 'contacts'
+}
 
 
 class TestHomeView(TestCase):
@@ -12,6 +22,10 @@ class TestHomeView(TestCase):
 
         # remember url to homepage
         self.url = reverse('hello:home')
+
+        self.sample = NORMAL
+        AboutMe.objects.all().delete()
+        self.first_profile = AboutMe.objects.create(**self.sample)
 
         # make request to the server to get homepage page
         self.response = self.client.get(self.url)
@@ -30,4 +44,33 @@ class TestHomeView(TestCase):
 
         # now check if we got proper email
         hello = self.response.context['bio']
-        self.assertEqual(hello['email'], 'k6_alexstr@ukr.net')
+        self.assertEqual(hello.email, 'k6_alexstr@ukr.net')
+
+    def test_first_entry_selection(self):
+        """ check first entry selection
+        when db have many entries"""
+
+        # create second AboutMe entry in the db
+        AboutMe.objects.create(**self.sample)
+
+        self.assertEqual(len(AboutMe.objects.all()), 2)
+
+        self.first_profile.first_name = 'Alex First'
+        self.first_profile.save()
+
+        self.response = self.client.get(self.url)
+
+        hello = self.response.context['bio']
+        self.assertEqual(hello.first_name, 'Alex First')
+
+    def test_no_entries_aboutme_in_db(self):
+        """ check correct reaction if there are
+        no aboutme entries in the db"""
+
+        AboutMe.objects.all().delete()
+        db = AboutMe.objects.all()
+        self.assertEqual(len(db), 0)
+
+        self.response = self.client.get(self.url)
+        self.assertTrue('There is no profile in the db'
+                        in self.response.content)
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,20 +1,8 @@
 from django.shortcuts import render
+from .models import AboutMe
 
 
 def home(request):
-    bio = {
-        'first_name': 'Oleksiy',
-        'last_name': 'Strutynskyy',
-        'birthday': 'May 7, 1979',
-        'photo': '',
-        'email': 'k6_alexstr@ukr.net',
-        'jabber': 'alexleon@42cc.co',
-        'skype': 'oleksiy.strutynskyy',
-        'contacts': 'facebook.com ' +
-                    'linkedin.com ' +
-                    'gmail.com',
-        'bio': 'I want to be Django Developer. ' +
-               'Django junior is my first aim.'
-    }
+    bio = AboutMe.objects.first()
 
     return render(request, 'home.html', {'bio': bio})
